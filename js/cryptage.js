db9879f587100c82daeb2d96dc76069588f44409
Med Amir Saadallah <amirs-m-s@live.com>
2018-03-02 00:59:54 +0000

    fix cryptage_engine module export

--- js/cryptage.js
+++ js/cryptage.js
@@ -1,7 +1,8 @@
 const crypto = require('crypto');
 const algorithm = 'aes-256-ctr';
-const password = 'd6F3Efeq'; // next will read it from process.env when hosted
-modules.exports = {
+const password = 'd6F3Efeq';
+// next will read it from process.env when hosted
+module.exports = {
   // Nodejs encryption with CTR
   encrypt(pass) {
     var cipher = crypto.createCipher(algorithm, password)
