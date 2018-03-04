23738b7feb52273bd1bf35a5ab248193b5da0ac4
Med Amir Saadallah <amirs-m-s@live.com>
2018-03-04 02:27:27 +0000

    token validation

--- controllers/TokenController.js
+++ controllers/TokenController.js
@@ -4,13 +4,38 @@ module.exports = {
     isTokenUp : function(req, res){
        // return True if token still up else return false
        let token = req.body.token
-       let id = req.body.id
-       Connection.find({id_user:id}).sort({create_date: -1}).exec(function(err,conx){
-         if (err) throw err
-         console.log(conx);
-         res.status(200).json({
-           all: conx
-         })
+       let date = req.body.date
+       //let pseudo = req.body.pseudo // use it to get user _id
+       Connection.findOne({'session_token':token}, function (err, cnx_one) {
+         if(err){
+           console.log(err)
+           throw err
+         }
+         if(cnx_one != null){
+           let user_id = cnx_one.id_user;
+           // get all cnx for that user -> sort theme -> find out if the token he have is equal to the last date
+           Connection.find({id_user:user_id}).sort({create_date: -1}).exec(function(err,conx){
+             if (err) {
+               console.log(err);
+               throw err
+             }
+             //console.log(conx);
+             let given_date = new Date(date);
+             let latest_date = new Date(conx[0].create_date.toString())
+             given_date.setSeconds(0);
+             given_date.setMilliseconds(0);
+             latest_date.setSeconds(0)
+             latest_date.setMilliseconds(0)
+             if (conx[0] != null && given_date.getTime() === latest_date.getTime() && token === conx[0].session_token) {
+               res.status(200).json({'isToeknUp':true})
+             } else {
+               res.status(200).json({'isToeknUp':false})
+             }
+           });
+         }
+         else{
+           res.status(200).json({'isToeknUp':false});
+         }
        });
     }
 }
